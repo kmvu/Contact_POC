bc830bd962a2fd05159eeb040840daa0ac265588
Khang Vu <kmvu91@gmail.com>
2022-02-23 10:15:52 +0700

    Initial Commit

---
+++ Contact_POC/Contact_POCApp.swift
@@ -0,0 +1,17 @@
+//
+//  Contact_POCApp.swift
+//  Contact_POC
+//
+//  Created by Khang Vu on 23/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct Contact_POCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
